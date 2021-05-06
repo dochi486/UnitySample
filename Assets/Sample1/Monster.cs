fa47f1404723f21e9a8b69bb9a25c48aff3d101f
hktrollgames <hktrollgames@gmail.com>
2021-05-06 09:18:22 +0900

    실행파일 빌드

--- Assets/Sample1/Monster.cs
+++ Assets/Sample1/Monster.cs
@@ -3,20 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Monster : MonoBehaviour
 {
     public Animator animator;
     public TextMeshPro hpNumber;
     public Transform hpBar;
-    public int maxHp;
+    public int maxHp = 3;
     public int currentHp;
+
+    private void Start()
+    {
+        maxHp = Random.Range(1, maxHp + 1);
+        currentHp = maxHp;
+        UpdateHPUI();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log(other);
 
-        StartCoroutine(OnDie());
+        StartCoroutine(OnHit());
+
+        // 화살 파괴
+        Destroy(other.gameObject);
     }
 
     public AudioSource dieSound;
@@ -24,6 +35,29 @@ public class Monster : MonoBehaviour
 
     public float dieDelay = 1.0f;
     public float speed = -5f;
+    private float delayAttackedMoveTime = 0.3f;
+
+    private IEnumerator OnHit()
+    {
+        currentHp -= 1;
+        // 화면의 HP갱신
+        UpdateHPUI();
+
+        bool isAlive = currentHp > 0;
+        if (isAlive)
+        {
+            animator.Play("GetHit");
+            //피격 애니메이션 움직이는거 일시 정지.
+            float originalSpeed = speed;
+            speed = 0;
+            yield return new WaitForSeconds(delayAttackedMoveTime);
+            speed = originalSpeed;
+        }
+        else
+        {
+            StartCoroutine(OnDie());
+        }
+    }
 
     private IEnumerator OnDie()
     {
@@ -37,6 +71,14 @@ public class Monster : MonoBehaviour
         animator.Play("Die");
         yield return new WaitForSeconds(dieDelay);
         Destroy(gameObject);
+    }
+
+    private void UpdateHPUI()
+    {
+        Vector3 scale = hpBar.localScale;
+        scale.x = currentHp / maxHp;
+        hpBar.localScale = scale;
+        hpNumber.text = $"{currentHp}/{maxHp}";
     }
 
     private IEnumerator PlayDieSound()
